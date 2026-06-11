1356fa4c4ca2e886f56d48a920e69bddfc01f638
Sneha Poojary <snehapoojary2004@gmail.com>
2026-06-11 12:00:24 +0530

    feat: restructure backend by removing old files and adding new modules for improved organization

--- Backend/app/registration/core/config.py
+++ Backend/app/registration/core/config.py
@@ -3,8 +3,10 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     GITHUB_TOKEN: str
-    
+
     class Config:
         env_file = ".env"
 
